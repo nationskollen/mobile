f89d2b106b67d5d11f0d023ee0355674f3fe172d
Oskar Rick <oskar.rick@gmail.com>
2021-04-01 12:10:06 +0200

    added icons to bottom bar

--- components/Bottom.js
+++ components/Bottom.js
@@ -2,22 +2,38 @@
 /// This component is used to get the bottom
 
 import React from 'react';
-import {View, Text, StyleSheet, Image, Button, TouchableOpacity, FlexStyle} from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
+import {View, Text, StyleSheet, Image, Button, TouchableOpacity, FlexStyle, TouchableHighlight} from 'react-native';
+import {AntDesign} from '@expo/vector-icons';
+import { SimpleLineIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Bottom = () => {
     return (
-        <View style = {{
-            backgroundColor: '#71002E',
-            height: 60,
-            width: '100%',
-            position: 'absolute',
-        }}>
-            <View/>
-            <View/>
-            <View/>
-            <View/>
-            <View/>
+        <View style = {styles.position}>
+            <View style = {styles.home}>
+                <SimpleLineIcons name="bell" size={25} color="white"/>
+                <Text style={styles.iconName}>Notifikationer</Text>
+            </View>
+            <View style = {styles.home}>
+                <SimpleLineIcons name="event" size={25} color="white"/>
+                <Text style={styles.iconName}>Bokningar</Text>
+            </View>
+            <View style = {styles.home}>
+                <SimpleLineIcons name="home" size={25} color="white"/>                
+                <Text style={styles.iconName}>Home</Text>
+            </View>
+            <View style = {styles.home}>
+                <SimpleLineIcons name="map" size={25} color="white"/>
+                <Text style={styles.iconName}>Karta</Text>
+            </View>
+            <View style = {styles.home}>
+                <TouchableOpacity>
+                <SimpleLineIcons name="settings" size={25} color="white" onPress={() => {
+                    alert('You touched me');
+                }}/>
+                </TouchableOpacity>
+                <Text style={styles.iconName}>Inställningar</Text>
+            </View>
         </View>
     );
 };
@@ -25,11 +41,24 @@ const Bottom = () => {
 const styles = StyleSheet.create({
     position: {
         backgroundColor: '#71002E',
-        height: 75,
-        alignSelf: 'flex-end',
+        height: 60,
+        width: '100%',
+        position: 'absolute',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexDirection: 'row'
     },
     home: {
-        
+        height: 50,
+        width: 66,
+        margin: 'auto',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    iconName: {
+        fontSize: 9,
+        marginTop: 3,
+        color: 'white'
     }
 
 });
