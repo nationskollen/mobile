1415bca3a60860830798717f159071c941992b4d
Robert Martinis <robertmartinis@hotmail.com>
2021-04-11 18:41:10 +0200

    Added Maps, API-keys, and markers for each nation

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -11,17 +11,24 @@ import "react-native-gesture-handler";
 import { HeaderOptions } from './NavigationHeader'
 import { useTheme } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import renderMap from '../components/Map/MapPage.js';
 
 const Stack = createStackNavigator();
 
 /// TODO: Create a local stack
 /// TODO: Import appropriate components (screens)
 function MapScreen() {
+    const { colors } = useTheme()
+
     return (
-        <View style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Map</Text>
-        </View>
+        <Stack.Navigator initialRouteName='Map' screenOptions={HeaderOptions(colors)}>
+            <Stack.Screen
+                name='Map'
+                component={renderMap}
+                options={{title: 'Karta'}}
+            />
+        </Stack.Navigator>
     );
-}
+};
 
 export default MapScreen;
