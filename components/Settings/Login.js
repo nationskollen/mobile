8e6b83845fe4dcc0473c34d91b0cc443f9d3ce12
Oskar Rick <oskar.rick@gmail.com>
2021-04-14 13:16:56 +0200

    Added a text input bar in login page

--- components/Settings/Login.js
+++ components/Settings/Login.js
@@ -1,17 +1,36 @@
 /// This is used to render login page
 
 import React from 'react';
-import { SafeAreaView, View, StyleSheet, Text, Button } from 'react-native';
+import { SafeAreaView, View, StyleSheet, Text, Button, TextInput } from 'react-native';
 import { useTheme } from '@react-navigation/native';
 
-function Login {
+function Login() {
     const { colors } = useTheme();
+    
+    const [text, onChangeText] = React.useState("Useless Text");
 
     return (
-	<View>
-	    
-	</View>
+	<View style={styles.container}>
+	    <TextInput
+		style={styles.input}
+		onChangeText={onChangeText}
+		value={text}
+		placeholder="Användarnamn"
+	    />
+    	</View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+	flex:1,
+	justifyContent: 'center',
+    },
+    input: {
+	height: 40,
+	margin: 10,
+	borderWidth: 1,
+    }
+})
 
 export default Login;
