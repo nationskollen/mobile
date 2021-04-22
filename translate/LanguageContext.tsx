a9bd9b4bd9746207841781529f5ce534274e6ec4
Robert Martinis <robertmartinis@hotmail.com>
2021-04-22 22:20:20 +0200

    Added english translation JSON

---
+++ translate/LanguageContext.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LanguageContext = React.createContext();
+
+export const LanguageContextProvider: React.FC = 
+    const.value = {};
+
+    return ( 
+	<LanguageContext.Provider value = {value}>
+	    { children }
+	</LanguageContext.Provider>
+    )
