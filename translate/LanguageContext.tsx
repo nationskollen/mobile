7983d96fa1061f42b7ec6a989dfdfc76c5dbb132
Robert Martinis <robertmartinis@hotmail.com>
2021-04-22 23:02:51 +0200

    Add language contexts to app

--- translate/LanguageContext.tsx
+++ translate/LanguageContext.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import en from './languages/en.json';
+import swe from './languages/swe.json';
+import * as RNLocalize from 'react-native-localize';
 
 const LanguageContext = React.createContext();
+const languages = {
+    en : en,
+    swe : swe,
+}
 
-export const LanguageContextProvider: React.FC = 
-    const.value = {};
+export const LanguageContextProvider: React.FC = ({children}) =>  {
+    // Standard langauge is Swedish so we set the first state to swe
+    const [languageSelected, setLanguageSelected] = useState('swe');
+    useEffect(() => {
+	const currentLanguage = RNLocalize.findBestAvailableLanguage(
+	    Object.keys(languages),
+	    );
+	
+	setLanguageSelected(currentLanguage?.languageTag || 'swe');
+	}, []);
 
+    const value = {...languages[languageSelected as 'en' | 'swe']};
     return ( 
 	<LanguageContext.Provider value = {value}>
 	    { children }
 	</LanguageContext.Provider>
     )
+}
+
+export const useTranslation = () => useContext(LanguageContext);
