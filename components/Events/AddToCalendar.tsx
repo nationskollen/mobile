723f1b234e4abb59fea4f3e918a6fbdd08cb048b
johannes.lil <jocka.liljedahl@gmail.com>
2021-04-29 10:05:23 +0200

    Added calendar sourceType, removed errors

--- components/Events/AddToCalendar.tsx
+++ components/Events/AddToCalendar.tsx
@@ -2,69 +2,60 @@
  * @category Home
  * @module AddToCalendar
  */
-
-import React, { useEffect, useState } from 'react';
-import {
-  Alert,
-  Platform,
-} from 'react-native';
-import * as Calendar from 'expo-calendar';
+import { Alert, Platform } from 'react-native'
+import * as Calendar from 'expo-calendar'
 
 import { Event } from '@dsp-krabby/sdk'
 import { RecurrenceRule, Alarm, Details } from './AddToCalendarInterface'
-
-//TODO: extract interfaces to separate file
-
 
 /**
  * Function used to export an event from the app to the native calendar on the used device
  * @param event event to be added to native calendar
  */
-async function addToCalendar (event:Event) {
+async function addToCalendar(event: Event) {
     const status = await getPermission()
     if (status != 'granted') {
         console.log(`Permission to use calendar: ${status}`)
         return
     }
 
-    var details:Details = getDetails(event)
+    var details: Details = getDetails(event)
     await createEvent(details)
 
     const calendars = await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT)
     console.log('Titles of existing calendars on device: ')
-    calendars.map((calendar)=>console.log(calendar.title + '\n'))
+    calendars.map((calendar) => console.log(calendar.title + '\n'))
 }
 
 /**
  * Function used to check/ask user for calendar permission
  * @return permission status (granted | undetermined | denied)
  */
-export async function getPermission() { //add type
+export async function getPermission() {
+    //add type
     const { status } = await Calendar.getCalendarPermissionsAsync()
     if (status != 'granted') {
         const { status } = await Calendar.requestCalendarPermissionsAsync()
 
         if (status == 'granted') {
-        
-        return status
-        }
-        else  {
-            Alert.alert("Error", "Please allow Calendar permissions to import event")
+            return status
+        } else {
+            Alert.alert('Error', 'Please allow Calendar permissions to import event')
         }
     }
     return status
 }
 
-const getDetails = (event:Event)=>{
-    return ({
+const getDetails = (event: Event): Details => {
+    return {
         title: event.name,
         startDate: new Date(event.occurs_at),
         endDate: new Date(event.ends_at),
+        notes: event.short_description,
         //location: useLocation(event.location_id) //incompatible type at the moment, compare types
-        //notes: event.shortDescription // type not found??
 
         //TODO: add more to event objects
-    })
+    }
 }
 
 /**
@@ -72,16 +63,17 @@ const getDetails = (event:Event)=>{
  * @param details object containing event information
  * @return new event id
  */
-async function createEvent(details:object) { // TODO: add strict type for details
-    const calendarID:string|null = await createCalendar()
-    
+async function createEvent(details: Details) {
+    // TODO: add strict type for details
+    const calendarID: string | null = await createCalendar()
+
     if (calendarID == null) {
-        console.log("could not find calendarID")
+        console.log('could not find calendarID')
         return
     }
 
     const newEventID = await Calendar.createEventAsync(calendarID, details)
-  
+
     return newEventID
 }
 
@@ -89,42 +81,50 @@ async function createEvent(details:object) { // TODO: add strict type for detail
  * Function used to fetch/create calendar
  * @return calendar id
  */
-async function createCalendar():Promise<string | null> {
+async function createCalendar(): Promise<string | null> {
     var calendarID = await getCalendarID()
-    
+
     // if calendar is not found, create new calendar
     if (calendarID == null) {
         const defaultCalendarSource =
-        Platform.OS === 'ios'
-            ? await getDefaultCalendarSource()
-            : { isLocalAccount: true, name: 'Expo Calendar' }
+            Platform.OS === 'ios'
+                ? await getDefaultCalendarSource()
+                : {
+                      isLocalAccount: true,
+                      name: 'Expo Calendar',
+                      type: Calendar.SourceType.SUBSCRIBED,
+                  } //Subscribed or Local calendar type?
         calendarID = await Calendar.createCalendarAsync({
-        title: 'Nationskollen Calendar',
-        color: '#71002E',
-        entityType: Calendar.EntityTypes.EVENT,
-        sourceId: defaultCalendarSource.id, // id does not exist in type? what should id be?
-        source: defaultCalendarSource, // unclear why vscode complains? code does not work without these parameters declared
-        name: 'internalCalendarName',
-        ownerAccount: 'personal',
-        accessLevel: Calendar.CalendarAccessLevel.OWNER,
+            title: 'Nationskollen Calendar',
+            color: '#71002E',
+            entityType: Calendar.EntityTypes.EVENT,
+            sourceId: defaultCalendarSource.id,
+            source: defaultCalendarSource,
+            name: 'internalCalendarName',
+            ownerAccount: 'personal',
+            accessLevel: Calendar.CalendarAccessLevel.OWNER,
         })
     }
 
-    calendarID == null && console.log("error: calendar not found nor created")
+    calendarID == null && console.log('error: calendar not found nor created')
     return calendarID
 }
 
-async function getCalendarID():Promise<string | null> {
+async function getCalendarID(): Promise<string | null> {
     const calendars = await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT)
     for (let i in calendars) {
-        if (calendars[i].title === 'Nationskollen Calendar' || calendars[i].name === 'Expo Calendar') return calendars[i].id
+        if (
+            calendars[i].title === 'Nationskollen Calendar' ||
+            calendars[i].name === 'Expo Calendar'
+        )
+            return calendars[i].id
     }
     return null
 }
 
 async function getDefaultCalendarSource() {
     const calendars = await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT)
-    const defaultCalendars = calendars.filter(each => each.source.name === 'Default')
+    const defaultCalendars = calendars.filter((each) => each.source.name === 'Default')
     return defaultCalendars[0].source
 }
 
--- components/Events/AddToCalendarInterface.tsx
+++ components/Events/AddToCalendarInterface.tsx
@@ -3,39 +3,40 @@
  * @category Home
  * @module AddToCalendarInterface
  */
+import * as Calendar from 'expo-calendar'
 
-interface RecurrenceRule {
-    frequency: string // Calendar.Frequency.DAILY/WEEKLY/MONTHLY/YEARLY
-    interval: number //if frequency is DAILY and interval is 2, event occurs every other day (defaults to 1)
-    endDate: Date //when recurrence stops
-    occurence:number //number of recurrences
-    //other options available for iOS, see documentation of expo-calendar
-}
+// interface RecurrenceRule {
+//     frequency: string // Calendar.Frequency.DAILY/WEEKLY/MONTHLY/YEARLY
+//     interval: number //if frequency is DAILY and interval is 2, event occurs every other day (defaults to 1)
+//     endDate: Date //when recurrence stops
+//     occurence:number //number of recurrences
+//     //other options available for iOS, see documentation of expo-calendar
+// }
 
-interface Alarm {
-    absoluteDate?: Date //iOS only
-    relativeOffset: number //num of minutes from startDate
-    method?: string //Android only
-}
-  
+// interface Alarm {
+//     absoluteDate?: Date //iOS only
+//     relativeOffset: number //num of minutes from startDate
+//     method?: string //Android only
+// }
+
 interface Details {
     title: string
-    startDate: Date 
+    startDate: Date
     endDate: Date
-    allDay?:boolean
+    allDay?: boolean
     location?: string
     notes?: string
-    alarms?: Array<Alarm>
-    recurrenceRule?: RecurrenceRule
+    alarms?: Array<Calendar.Alarm>
+    recurrenceRule?: Calendar.RecurrenceRule
     availability?: string
     timeZone?: string // Required on Android.
     endTimeZone?: string // Android only
     url?: string //iOS only
     organizerEmail?: string // Android only
     accessLevel?: string // Android only
-    guestsCanModify?: boolean// Android only
+    guestsCanModify?: boolean // Android only
     guestsCanInviteOthers?: boolean // Android only
     guestsCanSeeGuests?: boolean // Android only
 }
 
-export { RecurrenceRule, Alarm, Details }
+export { Details }
