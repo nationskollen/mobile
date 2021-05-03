f61ace76e6ea5479266b874235c8760c6be10d5d
Robert Martinis <robertmartinis@hotmail.com>
2021-05-03 15:20:38 +0200

    Add types to languagepage, formatting

--- components/Settings/LanguagePage.tsx
+++ components/Settings/LanguagePage.tsx
@@ -1,35 +1,36 @@
 import React, { useState } from 'react'
 import 'react-native-gesture-handler'
 import { useTheme } from '../ThemeContext'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text } from 'react-native'
 import { useTranslation } from '../../translate/LanguageContext'
-import ListButton from '../ListButton'
 import en from '../../translate/languages/en'
 import swe from '../../translate/languages/swe'
 import FocusAwareStatusBar from '../FocusAwareStatusBar'
 import { CheckBox } from 'react-native-elements'
+
+import LanguageContextType from '../../translate/LanguageContextType'
+
 var checkedStates = [
     { key: 0, name: 'English', checked: false, value: en },
     { key: 1, name: 'Svenska', checked: true, value: swe },
 ]
-
-var initialState = 1;
+var initialState = 1
 
 const LanguagePage = () => {
     const { colors } = useTheme()
     const { setSelectedLanguage } = useTranslation()
     const [currentlyChecked, setCurrentlyChecked] = useState(initialState)
 
-       const uncheckPreviousCheckbox = (key : any) => {
-	checkedStates[key].checked = false;
-       }
+    const uncheckPreviousCheckbox = (key: number) => {
+        checkedStates[key].checked = false
+    }
 
-    const checkSelectedCheckbox = (selectedLanguage : any, key : number) => {
-	setSelectedLanguage(selectedLanguage)
-    	checkedStates[key].checked = true;	
-	uncheckPreviousCheckbox(currentlyChecked)
-	setCurrentlyChecked(key)
-	initialState = key;
+    const checkSelectedCheckbox = (selectedLanguage: LanguageContextType, key: number) => {
+        setSelectedLanguage(selectedLanguage)
+        checkedStates[key].checked = true
+        uncheckPreviousCheckbox(currentlyChecked)
+        setCurrentlyChecked(key)
+        initialState = key
     }
 
     return (
@@ -37,15 +38,29 @@ const LanguagePage = () => {
             <FocusAwareStatusBar backgroundColor={colors.primary} />
             {checkedStates.map((option) => (
                 <CheckBox
-		    center
-		    size={24}
-		    checkedColor= '#71002E'
-		    title={<Text style={{flex:1, color: colors.text, fontSize: 16, fontWeight: 'bold'}}>{option.name}</Text>}
-		    fontFamily='Roboto'
+                    center
+                    size={24}
+                    checkedColor="#71002E"
+                    title={
+                        <Text
+                            style={{
+                                flex: 1,
+                                color: colors.text,
+                                fontSize: 16,
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            {option.name}
+                        </Text>
+                    }
+                    fontFamily="Roboto"
                     checked={option.checked}
                     iconRight
-                    onPress={() => checkSelectedCheckbox(option.value, option.key) }
-		    containerStyle = {{backgroundColor: colors.background, borderColor: colors.border}}
+                    onPress={() => checkSelectedCheckbox(option.value, option.key)}
+                    containerStyle={{
+                        backgroundColor: colors.background,
+                        borderColor: colors.border,
+                    }}
                 />
             ))}
         </View>
