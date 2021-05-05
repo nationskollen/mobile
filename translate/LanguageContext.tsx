9362d0e7d556e8647b3a4f4769805d85480d6cf0
Robert Martinis <57859068+RobertMartinis@users.noreply.github.com>
2021-05-05 20:49:45 +0200

    Added property to language hook for getting the selected language (#113)
    * Add get current language hook
    
    * Add so that you can only set language to a valid code
    
    * Update translate/LanguageContext.tsx
    
    Co-authored-by: Fredrik Engstrand <frewacom@gmail.com>

--- translate/LanguageContext.tsx
+++ translate/LanguageContext.tsx
@@ -2,9 +2,13 @@ import swe from './languages/swe'
 import React, { useState, useContext } from 'react'
 import LanguageContextType from './LanguageContextType'
 
+type LangCode  = 'en' | 'sv'
+
 export interface LanguageContextContract {
     setSelectedLanguage: React.Dispatch<React.SetStateAction<LanguageContextType>>
+    setCurrentLanguage: React.Dispatch<React.SetStateAction<LangCode>>
     translate: LanguageContextType
+    currentLanguage : LangCode 
 }
 const LanguageContext = React.createContext<LanguageContextContract>({} as LanguageContextContract)
 
@@ -12,9 +16,10 @@ export const useTranslation = () => useContext(LanguageContext)
 
 export const LanguageContextProvider = ({ children }) => {
     const [selectedLanguage, setSelectedLanguage] = useState(swe)
+    const [currentLanguage, setCurrentLanguage] = useState<LangCode>('sv')
 
     return (
-        <LanguageContext.Provider value={{ setSelectedLanguage, translate: selectedLanguage }}>
+        <LanguageContext.Provider value={{ setSelectedLanguage, translate: selectedLanguage, currentLanguage, setCurrentLanguage}}>
             {children}
         </LanguageContext.Provider>
     )
