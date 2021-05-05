69a9712680353e68d2cea90795de72d993cabe53
Robert Martinis <robertmartinis@hotmail.com>
2021-05-05 20:33:00 +0200

    Add so that you can only set language to a valid code

--- translate/LanguageContext.tsx
+++ translate/LanguageContext.tsx
@@ -2,11 +2,13 @@ import swe from './languages/swe'
 import React, { useState, useContext } from 'react'
 import LanguageContextType from './LanguageContextType'
 
+type LangCode  = 'en' | 'sv'
+
 export interface LanguageContextContract {
     setSelectedLanguage: React.Dispatch<React.SetStateAction<LanguageContextType>>
     setCurrentLanguage: React.Dispatch<React.SetStateAction<string>>
     translate: LanguageContextType
-    currentLanguage : string
+    currentLanguage : LangCode 
 }
 const LanguageContext = React.createContext<LanguageContextContract>({} as LanguageContextContract)
 
@@ -14,7 +16,7 @@ export const useTranslation = () => useContext(LanguageContext)
 
 export const LanguageContextProvider = ({ children }) => {
     const [selectedLanguage, setSelectedLanguage] = useState(swe)
-    const [currentLanguage, setCurrentLanguage] = useState('sv')
+    const [currentLanguage, setCurrentLanguage] = useState<LangCode>('sv')
 
     return (
         <LanguageContext.Provider value={{ setSelectedLanguage, translate: selectedLanguage, currentLanguage, setCurrentLanguage}}>
