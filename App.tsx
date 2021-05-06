404d147042d1db5cf3743ed5510f5d6a15e103a7
Fredrik Engstrand <frewacom@gmail.com>
2021-05-06 22:14:11 +0200

    Added push token generation

--- App.tsx
+++ App.tsx
@@ -3,21 +3,85 @@
  * @category BIG
  * @module App
  */
-
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import { Platform } from 'react-native'
 import 'react-native-gesture-handler'
 import Constants from 'expo-constants'
 import AppLoading from 'expo-app-loading'
 import { Provider } from '@dsp-krabby/sdk'
+import * as Notifications from 'expo-notifications'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { LanguageContextProvider } from './translate/LanguageContext'
 import { DarkTheme, LightTheme, ThemeProvider, Theme } from './components/ThemeContext'
 
 import Footer from './components/Footer/Footer'
 
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
+})
+
+async function registerForPushNotificationsAsync() {
+    let token: string
+
+    if (Constants.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync()
+        let finalStatus = existingStatus
+
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+
+        if (finalStatus !== 'granted') {
+            console.log('Failed to get push token for push notification!');
+            return;
+        }
+
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+    } else {
+        console.log('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+        });
+    }
+
+    return token;
+}
+
 const App = () => {
-    const [initialTheme, setInitialTheme] = useState<Theme | null>(null)
+    const responseListener = useRef<any>();
+    const notificationListener = useRef<any>();
     const [isReady, setIsReady] = useState(false)
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [initialTheme, setInitialTheme] = useState<Theme | null>(null)
+
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+            console.log(notification)
+        })
+
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log(response)
+        })
+
+        return () => {
+            Notifications.removeNotificationSubscription(notificationListener.current)
+            Notifications.removeNotificationSubscription(responseListener.current)
+        }
+    }, [])
 
     if (!isReady) {
         return (
@@ -37,7 +101,7 @@ const App = () => {
                 autoHideSplash={true}
                 onError={console.warn}
             />
-        )
+            )
     }
 
     return (
