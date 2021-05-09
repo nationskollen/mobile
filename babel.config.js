fab749687a3ffd61107f9844cae9281c4a004225
Robert Martinis <robertmartinis@hotmail.com>
2021-05-09 22:16:37 +0200

    Changed global font to Noto Sans

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
 module.exports = function (api) {
     api.cache(true);
+plugins : [
+    [
+	'module resolver',
+	{
+	    alias : {
+assets : './assets/' 
+},
+},
+],
+] 
     return {
         presets: ["babel-preset-expo"],
     };
 };
+
+
