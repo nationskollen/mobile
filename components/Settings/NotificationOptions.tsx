6c08b33a479c9bee158b6d09affc459eb5ff1050
Fredrik Engstrand <frewacom@gmail.com>
2021-05-11 12:54:47 +0200

    Rendered subscriptions topics dynamically

--- components/Settings/NotificationOptions.tsx
+++ components/Settings/NotificationOptions.tsx
@@ -2,33 +2,77 @@
  * @category Settings
  * @module NotificationOptions
  */
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { useTheme } from '../ThemeContext'
+import { useAsyncCallback } from 'react-async-hook'
 import { View, StyleSheet, Text } from 'react-native'
 import ToggleSwitch from 'toggle-switch-react-native'
+import { useApi, SubscriptionTopicCollection, SubscriptionCreateData } from '@nationskollen/sdk'
 import { useTranslation } from '../../translate/LanguageContext'
+import LanguageContextType from '../../translate/LanguageContextType'
+
+export type ToggleCallback = (toggled: boolean, topicId: number) => void
+
+export interface Props {
+    oid: number
+    token: string
+    topics: SubscriptionTopicCollection
+}
 
 export interface ToggleProps {
-    // `text` is the name/title of the toggle
     text: string
+    oid: number
+    topicId: number
+    token: string
+}
+
+function translateTopic(topicName: string, translate: LanguageContextType) {
+    switch (topicName) {
+        case 'events':
+            return translate.settings.notificationsetting.events
+        case 'news':
+            return translate.settings.notificationsetting.news
+        default:
+            return topicName
+    }
 }
 
 /// Renders the different notification options
-const NotificationOptions = () => {
+const NotificationOptions = ({ topics, oid, token }: Props) => {
     const { translate } = useTranslation()
-    // These will be dynamic, so no need to translate for now
+
     return (
         <View style={styles.options}>
-            <Toggle text={translate.settings.notificationsetting.events} />
-            <Toggle text={translate.settings.notificationsetting.news} />
+            {topics.map((topic) => (
+                <Toggle
+                    key={topic.id}
+                    token={token}
+                    oid={oid}
+                    topicId={topic.id}
+                    text={translateTopic(topic.name, translate)}
+                />
+            ))}
         </View>
     )
 }
 
-/// Renders toggle switches
-const Toggle = ({ text }: ToggleProps) => {
+// TODO: How should we set the initial state?
+const Toggle = ({ text, topicId, oid, token }: ToggleProps) => {
+    const api = useApi()
     const { colors } = useTheme()
-    const [toggle, setToggle] = useState(false)
+    const [toggled, setToggled] = useState(false)
+    const createAction = useAsyncCallback((data: SubscriptionCreateData) => api.subscriptions.create(data))
+    const deleteAction = useAsyncCallback((uuid: string) => api.subscriptions.delete(uuid))
+
+    const toggleCallback = useCallback((toggled: boolean) => {
+        setToggled(toggled)
+
+        if (toggled) {
+            createAction.execute({ oid, topic: topicId, token })
+        } else {
+            deleteAction.execute('xxx')
+        }
+    }, [topicId, oid, token])
 
     return (
         <View style={[styles.switch, { borderBottomColor: colors.border }]}>
@@ -37,11 +81,11 @@ const Toggle = ({ text }: ToggleProps) => {
                 <Text style={[styles.option, { color: colors.text }]}>{text}</Text>
             </View>
             <ToggleSwitch
-                isOn={toggle}
+                isOn={toggled}
                 onColor="#05c46b"
                 offColor={colors.borderDark}
                 size="large"
-                onToggle={() => setToggle(!toggle)}
+                onToggle={() => toggleCallback(!toggled)}
             />
         </View>
     )
--- components/Settings/NotificationSettings.tsx
+++ components/Settings/NotificationSettings.tsx
@@ -3,8 +3,9 @@
  * @module NotificationSettings
  */
 import React from 'react'
-import { FlatList } from 'react-native'
-import { useNations } from '@nationskollen/sdk'
+import { FlatList, Text } from 'react-native'
+import { usePushToken } from '../PushTokenContext'
+import { useNations, useSubscriptionTopics } from '@nationskollen/sdk'
 
 import Dropdown from '../Common/Dropdown'
 import ListEmpty from '../List/ListEmpty'
@@ -13,18 +14,46 @@ import NationLogo from '../Nations/Front/NationLogo'
 import NotificationOptions from './NotificationOptions'
 
 const NotificationSettings = () => {
+    const {
+        data: topics,
+        isValidating: isValidatingTopics,
+        mutate: mutateTopics
+    } = useSubscriptionTopics()
+    const { token } = usePushToken()
     const { data, error, isValidating, mutate } = useNations()
+
+    // TODO: Print error if push token could not be retrieved
+    if (!token) {
+        return null
+    }
+
+    // If no subscription topics can be found, we can not modify notifications
+    if ((!topics || topics.length === 0) && !isValidatingTopics) {
+        return <Text>Inga notifikationer tillg'ngliga</Text>
+    }
 
     return (
         <FlatList
             data={data}
             renderItem={({ item }) => (
                 <Dropdown title={item.name} icon={<NationLogo src={item.icon_img_src} />}>
-                    <NotificationOptions />
+                    <NotificationOptions
+                        oid={item.oid}
+                        token={token}
+                        topics={topics}
+                    />
                 </Dropdown>
             )}
             keyExtractor={(item) => item.oid.toString()}
-            refreshControl={<LoadingCircle validating={isValidating} mutate={mutate} />}
+            refreshControl={
+                <LoadingCircle
+                    validating={isValidating}
+                    mutate={() => {
+                        mutate()
+                        mutateTopics()
+                    }}
+                />
+            }
             ListEmptyComponent={() =>
                 ListEmpty({
                     error,
