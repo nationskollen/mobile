9c90b24729e69dc6b613ec728d3f7ff433f4e239
RobertMartinis <RobertMartinis@users.noreply.github.com>
2021-06-17 12:23:01 +0000

    Formatted code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        "ecmaVersion": 12,
-        "sourceType": "module",
+        ecmaVersion: 12,
+        sourceType: 'module',
     },
-    "plugins": [
-        "react",
-        "react-hooks",
-        "@typescript-eslint",
-    ],
-    "rules": {
+    plugins: ['react', 'react-hooks', '@typescript-eslint'],
+    rules: {},
+    settings: {
+        react: {
+            version: 'detect',
+        },
     },
-    "settings": {
-      react: {
-        version: "detect",
-      }
-
-    }
-};
+}
